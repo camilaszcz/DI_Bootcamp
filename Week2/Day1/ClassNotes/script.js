9c2fe96e0c8f5026dc331f148fafd9da6196b260
camilaszcz <camila.szczerbacki@gmail.com>
2022-10-18 13:34:42 +0300

    Week2 files

---
+++ Week2/Day1/ClassNotes/script.js
@@ -0,0 +1,10 @@
+const addressNumber = 44;
+const addressStreet = "YonaHanavi";
+const country = "Israel";
+
+const globalAddress = "I live in" + addressNumber + addressStreet + country;
+const globalAddress2 = `I live in ${addressNumber} ${addressStreet} ${country}`;
+
+console.log (globalAddress);
+console.log (globalAddress2);
+
