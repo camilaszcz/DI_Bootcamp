f810311ab2fd5ed55739aaea69a0547bed1af545
camilaszcz <camila.szczerbacki@gmail.com>
2022-10-20 08:22:12 +0300

    Update script.js

--- Week2/Day2/DailyChallenge/script.js
+++ Week2/Day2/DailyChallenge/script.js
@@ -6,10 +6,18 @@ let sentence="The food is not bad here!Let's come back.";
 let wordNot= sentence.search('not'); // result 12
 let wordBad= sentence.search('bad'); // result 16
 
-// so 'not bad' is from 12 to 18.
+// 'not bad' is from 12 to 18.
 // bad is after good, as 18 is after 12.
+//to make sure:
 
-let newSentence=sentence.replace("not bad", "good");
+if (wordBad>wordNot){
+    console.log('wordBad:'wordBad)
+    console.log('wordNot:', wordNot)
 
-console.log(newSentence);
+    let part1 = sentence.slice(0, wordNot)
+    let part2 = sentence.slice(wordBad+3)
+    console.log(part1 + "good" + part2)
+} else{
+    console.log(sentence)
+}
 //result: 'The food is good here!Let's come back.'
