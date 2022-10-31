d0bcbeba03e2d4d2064e791cd25073e9e12282c9
camilaszcz <camila.szczerbacki@gmail.com>
2022-10-31 09:57:25 +0200

    Week4Day1dailyChallenge

---
+++ Week4/Day1/DailyChallenge/main.py
@@ -0,0 +1,30 @@
+# Using the input function, ask the user for a string. The string must be 10 characters long.
+from calendar import c
+
+
+string = input('Give me a 10 letter word!')
+# If it’s less than 10 characters, print a message which states “string not long enough”.
+if len(string)<10:
+    print('Word not long enough')
+    # If it’s more than 10 characters, print a message which states “string too long”.
+elif len(string)>10:
+    print('Word too long')
+    # Then, print the first and last characters of the given text.
+else:
+    print('First char:', string[0])
+    print('Last char:', string[-1])
+    # temp_str=''
+    # for x  in string:
+    #     temp_str+=c
+    #     print(temp_str)
+        
+    for i in range(len(string)):
+    print(string[:i+1])
+# Using a for loop, construct the string character by character: 
+# Print the first character, then the second, then the third, until the full string is printed. 
+
+# for x in string(0,9,1):
+#     print(x)
+    
+
+    
