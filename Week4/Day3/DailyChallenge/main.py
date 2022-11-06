0104bd1b3db3f65d243013edb5674bbd62a91268
camilaszcz <camila.szczerbacki@gmail.com>
2022-11-06 20:18:26 +0200

    Week4Day3incomplete

---
+++ Week4/Day3/DailyChallenge/main.py
@@ -0,0 +1,22 @@
+# Challenge 1
+
+# Ask a user for a word
+
+# Write a program that creates a dictionary. This dictionary stores the indexes of each letter in a list.
+
+# str= input('Tell me a word')
+
+# my_dict = {}
+
+# my_dict = {letter : [] for letter in str}   #creates dict with keys being unique values in a
+# for i,letter in enumerate(str):
+#     my_dict[letter].append(i)        #adds each index into dict
+
+# print(my_dict)
+
+# Challenge 2// Will come back to it
+
+# Create a program that prints a list of the items you can afford in the store with the money you have in your wallet.
+# Sort the list in alphabetical order.
+# Return “Nothing” if you can’t afford anything from the store.
+
--- Week4/Day3/ExercisesXP/main.py
+++ Week4/Day3/ExercisesXP/main.py
@@ -13,7 +13,8 @@
 # print(dictionary)
 
 
-# Exercise 2 : Cinema
+# Exercise 2 : Cinema 
+# //////Got completely stuck! something happened in the middle and I couldnt come back from it!/////
 
 # A movie theater charges different ticket prices depending on a person’s age.
 # if a person is under the age of 3, the ticket is free.
@@ -26,21 +27,19 @@
 # Bonus: Ask the user to input the names and ages instead of using the provided family variable 
 # (Hint: ask the user for names and ages and add them into a family dictionary that is initially empty).
 
-cinema = {}
+# cinema = {}
 
-for i in range(4):
-    name = input("Enter your name: ")
-    age = input("Enter your age: ")
+# for i in range(4):
+#     name = input("Enter your name: ")
+#     age = input("Enter your age: ")
 
-cinema[name]=age
+# cinema[name]=age
 
-print(cinema)
-
+# print(cinema)
 
 # How much does each family member have to pay ?
 
 # price_list = cinema.values()
-
 
 # if cinema[age] < 3:
 #         print("Your ticket is free")
@@ -50,49 +49,63 @@ print(cinema)
 #         print("Your ticket is $15")
 # print(cinema)
 
-
 # total_cost = sum()
 
 # Print out the family’s total cost for the movies.
 
-
 # Exercise 3: Zara
-
-# Here is some information about a brand.
-# name: Zara 
-# creation_date: 1975 
-# creator_name: Amancio Ortega Gaona 
-# type_of_clothes: men, women, children, home 
-# international_competitors: Gap, H&M, Benetton 
-# number_stores: 7000 
-# major_color: 
-#     France: blue, 
-#     Spain: red, 
-#     US: pink, green
-
-
 # 2. Create a dictionary called brand which value is the information from part one (turn the info into keys and values).
-# 3. Change the number of stores to 2.
-# 4. Print a sentence that explains who Zaras clients are.
-# 5. Add a key called country_creation with a value of Spain.
-# 6. Check if the key international_competitors is in the dictionary. If it is, add the store Desigual.
-# 7. Delete the information about the date of creation.
-# 8. Print the last international competitor.
-# 9. Print the major clothes colors in the US. 
-# 10. Print the amount of key value pairs (ie. length of the dictionary).
-# 11. Print the keys of the dictionary.
-# 12. Create another dictionary called more_on_zara with the following details:
+brand= {
+'name': 'Zara',
+'creation_date': 1975, 
+'creator_name': 'Amancio' 'Ortega' 'Gaona' ,
+'type_of_clothes': ['men', 'women', 'children', 'home'],
+'international_competitors': ['Gap', 'HM', 'Benetton' ],
+'number_stores': 7000 ,
+'major_color': {
+    'France': 'blue', 
+    'Spain': 'red', 
+    'US': ['pink', 'green']},
+}
 
+# 3. Change the number of stores to 2.
+brand['number_stores']= 2
+
+# 4. Print a sentence that explains who Zaras clients are.
+print(','.join(brand['type_of_clothes']))
+# 5. Add a key called country_creation with a value of Spain.
+brand['country_creation'] = 'Spain',
+# 6. Check if the key international_competitors is in the dictionary. If it is, add the store Desigual.
+if 'international_competitors' in brand:
+    brand['international_competitors'].append('Desigual')
+# 7. Delete the information about the date of creation.
+del brand['creation_date']
+# 8. Print the last international competitor.
+print((brand['international_competitors'][-1]))
+# 9. Print the major clothes colors in the US. 
+print(brand['major_color']['US'])
+# 10. Print the amount of key value pairs (ie. length of the dictionary).
+print(len(brand))
+# 11. Print the keys of the dictionary.
+print(brand.keys())
+# 12. Create another dictionary called more_on_zara with the following details:
 # creation_date: 1975 
 # number_stores: 10 000
 
 
 # 13. Use a method to add the information from the dictionary more_on_zara to the dictionary brand.
+more_on_zara={
+    'creation_date':1975,
+    'number_stores':10000,
+    }
+brand.update(more_on_zara)
 # 14. Print the value of the key number_stores. What just happened ?
 
+print(brand['number_stores'])
+#Result 10000
 
 
-# Exercise 4 : Disney Characters
+# Exercise 4 : Disney Characters//Will come back to it
 
 # Use this list :
 
