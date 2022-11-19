a85b82966f8cae04e4380efa528afea1abaad229
camilaszcz <camila.szczerbacki@gmail.com>
2022-11-19 18:46:31 +0200

    Week5Day5_Rock_Paper_Scissors
    Mini project. completed the game part, missing the menu and score store

---
+++ Week5/Day5/MiniProject1/game.py
@@ -0,0 +1,50 @@
+# game.py – this file/module should contain a class called Game. It should have 4 methods:
+# get_user_item(self) – Ask the user to select an item (rock/paper/scissors). Keep asking until the user has selected one of the items – use data validation and looping. Return the item at the end of the function.
+
+# get_computer_item(self) – Select rock/paper/scissors at random for the computer. Return the item at the end of the function. Use python’s random.choice() function (read about it online).
+
+# get_game_result(self, user_item, computer_item) – Determine the result of the game.
+# Parameters:
+# user_item – the user’s chosen item (rock/paper/scissors)
+# computer_item – the computer’s chosen (random) item (rock/paper/scissors)
+# Return either win, draw, or loss. Where win means that the user has won, draw means the user and the computer got the same item, and loss means that the user has lost.
+
+# play(self) – the function that will be called from outside the class (ie. from rock-paper-scissors.py). It will do 3 things:
+# Get the user’s item (rock/paper/scissors) and remember it
+
+# Get a random item for the computer (rock/paper/scissors) and remember it
+
+# Determine the results of the game by comparing the user’s item and the computer’s item
+# Print the output of the game; something like this: “You selected rock. The computer selected paper. You lose”, “You selected scissors. The computer selected scissors. You drew!”
+
+# Return the results of the game as a string: win;draw;loss;, where win means that the user has won, draw means the user and the computer got the same item, and loss means that the user has lost.
+
+import random
+
+while True:
+    user_action = input("Enter a choice (rock, paper, scissors): ")
+    possible_actions = ["rock", "paper", "scissors"]
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "rock":
+        if computer_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if computer_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if computer_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
